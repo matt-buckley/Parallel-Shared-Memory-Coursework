e0342d7ad411046ecdb2a7493e5a0119919beab3
matt-buckley <29579469+matt-buckley@users.noreply.github.com>
2020-11-19 01:05:45 +0000

    Parallelise scalabilityTest.sh (multi-node)

--- scalabilityTest.sh
+++ scalabilityTest.sh
@@ -10,33 +10,30 @@ do
         for priority in 1.0 0.1 0.01 0.001 0.0001 0.00001 0.000001
         do
 
-            rm singleScaleTest.slurm
-            echo "#!/bin/bash" >> singleScaleTest.slurm
-            echo "#SBATCH --account=cm30225" >> singleScaleTest.slurm
-            echo "#SBATCH --partition=teaching" >> singleScaleTest.slurm
-            echo "#SBATCH --job-name=Parallel-$arraySize-$priority-$threadNum" >> singleScaleTest.slurm
-            echo "#SBATCH --time=00:15:00" >> singleScaleTest.slurm
-            echo "#SBATCH --output=parallel-results-$arraySize-$priority-$threadNum.out" >> singleScaleTest.slurm
-            echo "#SBATCH --nodes=1" >> singleScaleTest.slurm
-            echo "perf stat -r 4 ./parallel.out $arraySize $priority $threadNum" >> singleScaleTest.slurm
-            sbatch singleScaleTest.slurm
+            echo "#!/bin/bash" >> singleScaleTest-$arraySize-$priority-$threadNum.slurm
+            echo "#SBATCH --account=cm30225" >> singleScaleTest-$arraySize-$priority-$threadNum.slurm
+            echo "#SBATCH --partition=teaching" >> singleScaleTest-$arraySize-$priority-$threadNum.slurm
+            echo "#SBATCH --job-name=Parallel-$arraySize-$priority-$threadNum" >> singleScaleTest-$arraySize-$priority-$threadNum.slurm
+            echo "#SBATCH --time=00:15:00" >> singleScaleTest-$arraySize-$priority-$threadNum.slurm
+            echo "#SBATCH --output=parallel-results-$arraySize-$priority-$threadNum.out" >> singleScaleTest-$arraySize-$priority-$threadNum.slurm
+            echo "#SBATCH --nodes=1" >> singleScaleTest-$arraySize-$priority-$threadNum.slurm
+            echo "perf stat -r 4 ./parallel.out $arraySize $priority $threadNum" >> singleScaleTest-$arraySize-$priority-$threadNum.slurm
+            sbatch singleScaleTest-$arraySize-$priority-$threadNum.slurm
 
-            rm singleScaleTest.slurm
-            echo "#!/bin/bash" >> singleScaleTest.slurm
-            echo "#SBATCH --account=cm30225" >> singleScaleTest.slurm
-            echo "#SBATCH --partition=teaching" >> singleScaleTest.slurm
-            echo "#SBATCH --job-name=Sequential-$arraySize-$priority" >> singleScaleTest.slurm
-            echo "#SBATCH --time=00:15:00" >> singleScaleTest.slurm
-            echo "#SBATCH --output=sequential-results-$arraySize-$priority-$threadNum.out" >> singleScaleTest.slurm
-            echo "#SBATCH --nodes=1" >> singleScaleTest.slurm
-            echo "perf stat -r 4 ./sequential.out $arraySize $priority" >> singleScaleTest.slurm
-            sbatch singleScaleTest.slurm
+            echo "#!/bin/bash" >> singleScaleTest-$arraySize-$priority.slurm
+            echo "#SBATCH --account=cm30225" >> singleScaleTest-$arraySize-$priority.slurm
+            echo "#SBATCH --partition=teaching" >> singleScaleTest-$arraySize-$priority.slurm
+            echo "#SBATCH --job-name=Sequential-$arraySize-$priority" >> singleScaleTest-$arraySize-$priority.slurm
+            echo "#SBATCH --time=00:15:00" >> singleScaleTest-$arraySize-$priority.slurm
+            echo "#SBATCH --output=sequential-results-$arraySize-$priority-$threadNum.out" >> singleScaleTest-$arraySize-$priority.slurm
+            echo "#SBATCH --nodes=1" >> singleScaleTest-$arraySize-$priority.slurm
+            echo "perf stat -r 4 ./sequential.out $arraySize $priority" >> singleScaleTest-$arraySize-$priority.slurm
+            sbatch singleScaleTest-$arraySize-$priority.slurm
 
         done
     done
 done
 
-rm singleScaleTest.slurm
 echo "#!/bin/bash" >> singleScaleTest.slurm
 echo "#SBATCH --account=cm30225" >> singleScaleTest.slurm
 echo "#SBATCH --partition=teaching" >> singleScaleTest.slurm
@@ -46,4 +43,3 @@ echo "#SBATCH --mail-type=END" >> singleScaleTest.slurm
 echo "#SBATCH --mail-user=mcb68@bath.ac.uk" >> singleScaleTest.slurm
 echo "echo finished" >> singleScaleTest.slurm
 sbatch singleScaleTest.slurm
-rm singleScaleTest.slurm
