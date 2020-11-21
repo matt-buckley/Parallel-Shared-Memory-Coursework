14648df3aabd7dfdc9ffa1625bccd47490e7f34e
matt-buckley <29579469+matt-buckley@users.noreply.github.com>
2020-11-21 16:09:11 +0000

    Revert "Update scalability test for smaller iteration size"
    This reverts commit ca17a2e986707924c1dc538201b36c30e4f57854.

--- findNumIterations.sh
+++ findNumIterations.sh
@@ -1,8 +1,8 @@
 gcc parallel.c -lpthread -o parallel.out
 
-precision=0.01
+precision=0.001
 
-for arraySize in 2000
+for arraySize in 100 200 400 600 800 900 1000
 do
 	echo "For $arraySize x $arraySize :"
 	./parallel.out $arraySize $precision 12
--- scalabilityTest.sh
+++ scalabilityTest.sh
@@ -3,30 +3,29 @@
 gcc parallel.c -lpthread -o parallel.out
 gcc sequential.c -o sequential.out
 
-#for arraySize in 10 100 200 400 600 800 900 1000
+for arraySize in 10 100 200 400 600 800 900 1000
 #for arraySize in 10 50 100 200 300 400 500 600 700 800 900 950 1000
 #for arraySize in 200 400 600 800
-for arraySize in 200 600 1000 1400 1800 2200 2300 2400 2500 2600
 do
-    for threadNum in 2 4 6 8 10 12 13 14 15 16 17 18 19 20 22 24 26 28 30 32
+    #for threadNum in 2 4 6 8 10 12 14 16
     #for threadNum in  1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
     #for threadNum in 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32
-    #for threadNum in 1
+    for threadNum in 1
     do
-        #for priority in 1.0 0.1 0.01 0.001 0.00001 0.0000001
+        for priority in 1.0 0.1 0.01 0.001 0.00001 0.0000001
         #for priority in 1.0 0.1 0.01 0.001 0.0001 0.00001 0.000001
-        for priority in 0.01
+        #for priority in 0.001
         do
 
-            echo "#!/bin/bash" >> singleScaleTest-$arraySize-$priority-$threadNum.slurm
-            echo "#SBATCH --account=cm30225" >> singleScaleTest-$arraySize-$priority-$threadNum.slurm
-            echo "#SBATCH --partition=teaching" >> singleScaleTest-$arraySize-$priority-$threadNum.slurm
-            echo "#SBATCH --job-name=Parallel-$arraySize-$priority-$threadNum" >> singleScaleTest-$arraySize-$priority-$threadNum.slurm
-            echo "#SBATCH --time=00:15:00" >> singleScaleTest-$arraySize-$priority-$threadNum.slurm
-            echo "#SBATCH --output=parallel-results-$arraySize-$priority-$threadNum.txt" >> singleScaleTest-$arraySize-$priority-$threadNum.slurm
-            echo "#SBATCH --nodes=1" >> singleScaleTest-$arraySize-$priority-$threadNum.slurm
-            echo "perf stat ./parallel.out $arraySize $priority $threadNum" >> singleScaleTest-$arraySize-$priority-$threadNum.slurm
-            sbatch singleScaleTest-$arraySize-$priority-$threadNum.slurm
+            #echo "#!/bin/bash" >> singleScaleTest-$arraySize-$priority-$threadNum.slurm
+            #echo "#SBATCH --account=cm30225" >> singleScaleTest-$arraySize-$priority-$threadNum.slurm
+            #echo "#SBATCH --partition=teaching" >> singleScaleTest-$arraySize-$priority-$threadNum.slurm
+            #echo "#SBATCH --job-name=Parallel-$arraySize-$priority-$threadNum" >> singleScaleTest-$arraySize-$priority-$threadNum.slurm
+            #echo "#SBATCH --time=00:15:00" >> singleScaleTest-$arraySize-$priority-$threadNum.slurm
+            #echo "#SBATCH --output=parallel-results-$arraySize-$priority-$threadNum.txt" >> singleScaleTest-$arraySize-$priority-$threadNum.slurm
+            #echo "#SBATCH --nodes=1" >> singleScaleTest-$arraySize-$priority-$threadNum.slurm
+            #echo "perf stat ./parallel.out $arraySize $priority $threadNum" >> singleScaleTest-$arraySize-$priority-$threadNum.slurm
+            #sbatch singleScaleTest-$arraySize-$priority-$threadNum.slurm
 
             echo "#!/bin/bash" >> singleScaleTest-$arraySize-$priority.slurm
             echo "#SBATCH --account=cm30225" >> singleScaleTest-$arraySize-$priority.slurm
